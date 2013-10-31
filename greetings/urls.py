d65656edbb11b69f22d6ffb7fcbd77cdbde80a9f
Takashi ITO <cortyuming@gmail.com>
2013-10-31 09:42:42 +0900

    Change the greetings app

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
-from greetings.views import GreetingListView, GreetingCreateView, GreetingUpdateView
+from greetings.views import GreetingCreateView
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', GreetingListView.as_view(), name='list'),
-    url(r'^new$', GreetingCreateView.as_view(), name='new'),
-    url(r'^edit/(?P<pk>\d+)$', GreetingUpdateView.as_view(), name='edit'),
+    # url(r'^$', GreetingListView.as_view(), name='list'),
+    url(r'^$', GreetingCreateView.as_view(), name='list'),
+    # url(r'^new$', GreetingCreateView.as_view(), name='new'),
 )
--- greetings/views.py
+++ greetings/views.py
@@ -1,21 +1,15 @@
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView
 
 from greetings.models import Greeting
-
-
-class GreetingListView(ListView):
-    model = Greeting
-    context_object_name = 'greetings'
-    template_name = 'greeting_list.html'
 
 
 class GreetingCreateView(CreateView):
     model = Greeting
     success_url = reverse_lazy('greetings:list')
+    template_name = 'greetings/greetings.html'
 
-
-class GreetingUpdateView(UpdateView):
-    model = Greeting
-    success_url = reverse_lazy('greetings:list')
-    template_name = 'greeting_form.html'
+    def get_context_data(self, **kwargs):
+        greetings = Greeting.objects.order_by('-date_created')
+        kwargs['greetings'] = greetings
+        return kwargs
