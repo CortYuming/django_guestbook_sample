d75b8be1608fc14f207b3bd015334b1f7a372700
Takashi ITO <cortyuming@gmail.com>
2013-10-31 15:43:37 +0900

    delete commentout

--- greetings/urls.py
+++ greetings/urls.py
@@ -5,7 +5,5 @@ from greetings.views import GreetingCreateView
 
 urlpatterns = patterns(
     '',
-    # url(r'^$', GreetingListView.as_view(), name='list'),
     url(r'^$', GreetingCreateView.as_view(), name='list'),
-    # url(r'^new$', GreetingCreateView.as_view(), name='new'),
 )
